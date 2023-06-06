5cb4599f047cc4ca93e97dc2c7a842b1b222450c
Loic-Dubrion <cochisecoicoin@gmail.com>
2023-06-06 19:09:48 +0200

    docs: :memo: swwagger for user/:userId

--- app/routers/API/user.js
+++ app/routers/API/user.js
@@ -5,6 +5,23 @@ const router = express.Router();
 const controllerHandler = require('../../controllers/services/controllerHandler');
 const { userController } = require('../../controllers/API');
 
+/**
+ * GET /api/user/{userId}/
+ *
+ * @summary Get profil and favorite countries for a user
+ * @tags Users - operations related to users
+ *
+ * @param {integer} userId.path.required - The ID of the user
+ *
+ * @return {object} 200 - success response - object representing the user's favorite countries
+ * @property {string} username - The username of the user
+ * @property {string} origin_country - The user's country of origin
+ * @property {array<string>} favorite_countries - The user's favorite countries
+ * @property {number} favorite_country_percentage -
+ * The percentage of countries that are favorited by the user
+ * @return {Error} 500 - Internal server error
+ *
+ */
 router.get(
   '/:userId',
   controllerHandler(userController.getFavoriteCountries.bind(userController)),
