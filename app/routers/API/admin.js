23de88f7633d45189769e638d88db04171acd939
Loic-Dubrion <cochisecoicoin@gmail.com>
2023-06-23 11:20:50 +0200

    test

--- app/routers/API/admin.js
+++ app/routers/API/admin.js
@@ -9,8 +9,11 @@ const { adminController } = require('../../controllers/API');
 // Middlewares
 const { checkRole, checkPermission } = require('../../controllers/services/checkRBAC');
 const { authorize } = require('../../controllers/services/jwtService');
+const validate = require('../../validations/validate');
+const { validateIdParam } = require('../../validations/schemas');
 
 // Check Role
+router.param('userId', validate(validateIdParam, 'params'));
 router.use('/:userId', authorize);
 
 /**
