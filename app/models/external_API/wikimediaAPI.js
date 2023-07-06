e64dd4adae8ff12a0f69f1c6171fee6755096ac6
Loic-Dubrion <cochisecoicoin@gmail.com>
2023-07-06 19:30:59 +0200

    build: :construction: api wikimedia

---
+++ app/models/external_API/wikimediaAPI.js
@@ -0,0 +1,78 @@
+require('dotenv').config();
+
+const axios = require('axios');
+const { Error503 } = require('../../errors');
+const redisClient = require('../../services/clientDB/clientRedis');
+
+const wikimedia = {
+  fetchCountryPictures: async (name) => {
+    // redis caching
+    await redisClient.connect();
+    const cacheKey = `wikimedia:${name}`;
+    const cacheValue = await redisClient.get(cacheKey);
+
+    // If cached data, I return them
+    if (cacheValue) {
+      await redisClient.quit();
+      return JSON.parse(cacheValue);
+    }
+
+    // query construction for the category
+    const baseUrl = 'https://commons.wikimedia.org/w/api.php?';
+    const param = {
+      action: 'query',
+      format: 'json',
+      list: 'categorymembers',
+      cmtitle: `Category:Landscapes of ${name}`,
+      cmtype: 'file',
+      cmlimit: 3, // limit to first 3 images
+    };
+
+    const url = `${baseUrl}action=${param.action}&format=${param.format}&list=${param.list}&cmtitle=${param.cmtitle}&cmtype=${param.cmtype}&cmlimit=${param.cmlimit}`;
+    console.log(url);
+
+    try {
+      const response = await axios.get(url);
+      const pages = response.data.query.categorymembers;
+      const imageInfoResponses = await Promise.all(pages.map(async (page) => {
+        const imageUrl = `${baseUrl}action=query&format=json&prop=imageinfo&iiprop=url|extmetadata&titles=${encodeURIComponent(page.title)}`;
+        return axios.get(imageUrl);
+      }));
+
+      const images = imageInfoResponses.map((res) => {
+        const { pages } = res.data.query;
+        const imageInfo = pages[Object.keys(pages)[0]].imageinfo[0];
+        const imageUrl = imageInfo.url;
+        const description = imageInfo.extmetadata.Description
+          ? imageInfo.extmetadata.Description.value
+          : 'No description available';
+        const artist = imageInfo.extmetadata.Artist
+          ? imageInfo.extmetadata.Artist.value
+          : 'No artist information available';
+        const dateTimeOriginal = imageInfo.extmetadata.DateTimeOriginal
+          ? imageInfo.extmetadata.DateTimeOriginal.value
+          : 'No date information available';
+        const usageTerms = imageInfo.extmetadata.UsageTerms
+          ? imageInfo.extmetadata.UsageTerms.value
+          : 'No usage terms available';
+        return {
+          imageUrl, description, artist, dateTimeOriginal, usageTerms,
+        };
+      });
+
+      // Caching with Redis
+      await redisClient.set(cacheKey, JSON.stringify(images));
+      redisClient.expire(cacheKey, process.env.REDIS_TTL);
+      await redisClient.quit();
+
+      return images;
+    } catch (error) {
+      if (redisClient) await redisClient.quit();
+      throw new Error503({ HttpCode: 503, Status: 'Fail', Message: 'Service Unavailable' });
+    }
+  },
+};
+
+wikimedia.fetchCountryPictures('Poland').then(console.log).catch(console.error);
+
+module.exports = wikimedia;
