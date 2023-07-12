6e0f8ac47b5445c02d8916d867621fa1f846ff32
Loic-Dubrion <cochisecoicoin@gmail.com>
2023-07-12 16:29:55 +0200

    fix: :bug: fix bug redis

--- index.js
+++ index.js
@@ -11,17 +11,3 @@ const server = http.createServer(app);
 server.listen(PORT, () => {
   logger.info(`HTTP server ready on port ${PORT}, => http://${process.env.PGHOST}:${PORT} 🚀🚀🚀`);
 });
-
-// const https = require('https');
-// const fs = require('fs');
-
-// const options = {
-//   key: fs.readFileSync('../etc/nginx/ssl-certificates/www.lodev.tech.key'),
-//   cert: fs.readFileSync('../etc/nginx/ssl-certificates/www.lodev.tech.crt')
-// };
-
-// const server = https.createServer(options, app);
-
-// server.listen(PORT, () => {
-//   logger.info(`HTTPS server ready on port ${PORT}`);
-// });
